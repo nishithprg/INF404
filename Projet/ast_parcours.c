1dff7258f164b0389b99fe4fe399f5f3b22bc337
nishithprg <puranik.2001@gmail.com>
2020-04-17 12:30:44 +0200

    Fichiers pour l'ast

---
+++ Projet/ast_parcours.c
@@ -0,0 +1,39 @@
+
+#include <stdio.h>
+#include "type_ast.h"
+
+void aff_operateur(TypeOperateur op){
+	switch (op) {
+		case N_PLUS:
+			printf("+") ;
+			break;
+		case N_MOINS:
+			printf("-") ;
+			break;
+		case N_MUL:
+			printf("*") ;
+			break;
+	} 
+}
+
+void afficher(Ast expr) {
+	switch (expr->nature) {
+               case OPERATION:
+                        aff_operateur(expr->operateur) ;
+                        printf("(");
+                        afficher(expr->gauche);
+                        printf(", ");
+                        afficher(expr->droite);
+                        printf(")");
+			break ;
+               case VALEUR:
+                  	printf("%d", expr->valeur);
+			break ;
+	}
+}
+
+int evaluation(Ast expr) {
+      // A COMPLETER !
+      return -1 ;
+}
+
